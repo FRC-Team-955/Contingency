124e1d7d15c7a389c4c6d13c4309d8396d5c34ca
Masterchef365 <duncan.freeman1@gmail.com>
2018-03-22 19:28:58 -0700

    Better scissor lift settings

--- src/include/common_settings.h
+++ src/include/common_settings.h
@@ -37,14 +37,15 @@ const int shift_counts_max = 72;
 // Limits
 const float scissorlift_one_rotation_nu = 4096.0;
 const float scissorlift_minumum_height_in = 0.0;
-const float scissorlift_maximum_height_in = -7.75;
+//const float scissorlift_maximum_height_in = -7.75;
+const float scissorlift_maximum_height_in = -8.75;
 const float scissorlift_in_to_nu = scissorlift_one_rotation_nu * 5.0;
 
 const float drbase_f_gain_left = 0.19121;
 const float drbase_f_gain_right = 0.19283;
 
 // Scissor lift coeffs
-const float scissorlift_p_gain = 0.3 / scissorlift_one_rotation_nu;
+const float scissorlift_p_gain = 1.4 / scissorlift_one_rotation_nu;
 const float scissorlift_max_speed = 1.0;
 
 #endif
